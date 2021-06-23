dc2bcb16d78a782baad8c481832c1a43858f851e
Tiana Lemesle <tiana@goomeo.com>
2021-06-23 18:03:16 +0200

    :construction: Add HTTP server

--- mod.js
+++ mod.js
@@ -1,1 +1,8 @@
+import pogo from 'https://deno.land/x/pogo@v0.5.2/main.ts';
 import { signJwt, verifyJwt } from './lib/jwt.js';
+
+const server = pogo.server({
+    port: parseInt(Deno.env.get('PORT'))
+});
+
+server.start();
